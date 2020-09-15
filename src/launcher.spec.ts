e0bff3681593d096a2fd65b8f578b20494e770a5
Erwin Heitzman <erwin.heitzman@gmail.com>
2020-09-15 17:37:26 +0200

    Tweak setup

--- src/launcher.spec.ts
+++ src/launcher.spec.ts
@@ -4,17 +4,17 @@ const existsSync = jest.fn();
 const spawn = jest.fn().mockReturnValue({ on: jest.fn() });
 
 jest.mock('child_process', () => ({
-    spawn,
+	spawn,
 }));
 
 jest.mock('tcp-port-used', () => ({
-    waitUntilUsed,
-    waitUntilFree,
+	waitUntilUsed,
+	waitUntilFree,
 }));
 
 jest.mock('fs', () => ({
-    existsSync,
-    mkdirSync: jest.fn(),
+	existsSync,
+	mkdirSync: jest.fn(),
 }));
 
 import { resolve } from 'path';
@@ -27,193 +27,186 @@ let capabilities: Capabilities;
 let wdioConfig: WdioConfig;
 
 beforeEach(() => {
-    options = {};
-    capabilities = [{ browserName: 'chrome' }];
-    wdioConfig = {};
-    defaultArgs = [
-        '-jar',
-        resolve(__dirname, 'wiremock-standalone-2.26.3.jar'),
-        '-port',
-        8080,
-        '-root-dir',
-        'wiremock',
-    ];
-    spawnOptions = { detached: true, stdio: 'inherit' };
+	options = {};
+	capabilities = [{ browserName: 'chrome' }];
+	wdioConfig = {};
+	defaultArgs = ['-jar', resolve(__dirname, 'wiremock-standalone-2.26.3.jar'), '-port', 8080, '-root-dir', 'wiremock'];
+	spawnOptions = { detached: true, stdio: 'inherit' };
 });
 
 afterEach(() => {
-    jest.clearAllMocks();
+	jest.clearAllMocks();
 });
 
 it('should start the service when no options are passed', async () => {
-    const launcher = new WiremockLauncher();
-    launcher.installFile = jest.fn();
+	const launcher = new WiremockLauncher();
+	launcher.installFile = jest.fn();
 
-    await launcher.onPrepare();
+	await launcher.onPrepare();
 
-    expect(spawn).toHaveBeenCalledTimes(1);
-    expect(spawn).toHaveBeenCalledWith('java', defaultArgs, spawnOptions);
+	expect(spawn).toHaveBeenCalledTimes(1);
+	expect(spawn).toHaveBeenCalledWith('java', defaultArgs, spawnOptions);
 });
 
 it('should start the service when a empty options is passed', async () => {
-    const launcher = new WiremockLauncher(options);
-    launcher.installFile = jest.fn();
+	const launcher = new WiremockLauncher(options);
+	launcher.installFile = jest.fn();
 
-    await launcher.onPrepare();
+	await launcher.onPrepare();
 
-    expect(spawn).toHaveBeenCalledTimes(1);
-    expect(spawn).toHaveBeenCalledWith('java', defaultArgs, spawnOptions);
+	expect(spawn).toHaveBeenCalledTimes(1);
+	expect(spawn).toHaveBeenCalledWith('java', defaultArgs, spawnOptions);
 });
 
 it('should assign custom port', async () => {
-    const launcher = new WiremockLauncher({ ...options, port: 9999 });
-    launcher.installFile = jest.fn();
-    defaultArgs[3] = 9999;
+	const launcher = new WiremockLauncher({ ...options, port: 9999 });
+	launcher.installFile = jest.fn();
+	defaultArgs[3] = 9999;
 
-    await launcher.onPrepare();
+	await launcher.onPrepare();
 
-    expect(spawn).toHaveBeenCalledTimes(1);
-    expect(spawn).toHaveBeenCalledWith('java', defaultArgs, spawnOptions);
+	expect(spawn).toHaveBeenCalledTimes(1);
+	expect(spawn).toHaveBeenCalledWith('java', defaultArgs, spawnOptions);
 });
 
 it('should assign custom rootDir', async () => {
-    const launcher = new WiremockLauncher({ ...options, rootDir: 'example' });
-    launcher.installFile = jest.fn();
+	const launcher = new WiremockLauncher({ ...options, rootDir: 'example' });
+	launcher.installFile = jest.fn();
 
-    defaultArgs[5] = 'example';
+	defaultArgs[5] = 'example';
 
-    await launcher.onPrepare();
+	await launcher.onPrepare();
 
-    expect(spawn).toHaveBeenCalledTimes(1);
-    expect(spawn).toHaveBeenCalledWith('java', defaultArgs, spawnOptions);
+	expect(spawn).toHaveBeenCalledTimes(1);
+	expect(spawn).toHaveBeenCalledWith('java', defaultArgs, spawnOptions);
 });
 
 it('should assign custom stdio', async () => {
-    const launcher = new WiremockLauncher({ ...options, stdio: 'ignore' });
-    launcher.installFile = jest.fn();
+	const launcher = new WiremockLauncher({ ...options, stdio: 'ignore' });
+	launcher.installFile = jest.fn();
 
-    await launcher.onPrepare();
+	await launcher.onPrepare();
 
-    expect(spawn).toHaveBeenCalledTimes(1);
-    expect(spawn).toHaveBeenCalledWith('java', defaultArgs, { ...spawnOptions, stdio: 'ignore' });
+	expect(spawn).toHaveBeenCalledTimes(1);
+	expect(spawn).toHaveBeenCalledWith('java', defaultArgs, { ...spawnOptions, stdio: 'ignore' });
 });
 
 it('should assign custom mavenBaseUrl', async () => {
-    const url = 'maven-url/com/github/tomakehurst/wiremock-standalone/2.26.3/wiremock-standalone-2.26.3.jar';
-    const launcher = new WiremockLauncher({ ...options, mavenBaseUrl: 'maven-url' });
-    launcher.installFile = jest.fn();
-    existsSync.mockReturnValue(false);
+	const url = 'maven-url/com/github/tomakehurst/wiremock-standalone/2.26.3/wiremock-standalone-2.26.3.jar';
+	const launcher = new WiremockLauncher({ ...options, mavenBaseUrl: 'maven-url' });
+	launcher.installFile = jest.fn();
+	existsSync.mockReturnValue(false);
 
-    await launcher.onPrepare();
+	await launcher.onPrepare();
 
-    expect(launcher.installFile).toHaveBeenCalledWith(url, defaultArgs[1]);
-    expect(spawn).toHaveBeenCalledTimes(1);
-    expect(spawn).toHaveBeenCalledWith('java', defaultArgs, spawnOptions);
+	expect(launcher.installFile).toHaveBeenCalledWith(url, defaultArgs[1]);
+	expect(spawn).toHaveBeenCalledTimes(1);
+	expect(spawn).toHaveBeenCalledWith('java', defaultArgs, spawnOptions);
 });
 
 it('should not install wiremock when skipWiremockInstall is set to true', async () => {
-    const launcher = new WiremockLauncher({ ...options, skipWiremockInstall: true });
-    launcher.installFile = jest.fn();
+	const launcher = new WiremockLauncher({ ...options, skipWiremockInstall: true });
+	launcher.installFile = jest.fn();
 
-    await launcher.onPrepare();
+	await launcher.onPrepare();
 
-    expect(launcher.installFile).toHaveBeenCalledTimes(0);
+	expect(launcher.installFile).toHaveBeenCalledTimes(0);
 });
 
 it('should install wiremock when skipWiremockInstall is set to false', async () => {
-    const launcher = new WiremockLauncher({ ...options, skipWiremockInstall: false });
-    launcher.installFile = jest.fn();
-    existsSync.mockReturnValue(false);
+	const launcher = new WiremockLauncher({ ...options, skipWiremockInstall: false });
+	launcher.installFile = jest.fn();
+	existsSync.mockReturnValue(false);
 
-    await launcher.onPrepare();
+	await launcher.onPrepare();
 
-    expect(launcher.installFile).toHaveBeenCalledTimes(1);
+	expect(launcher.installFile).toHaveBeenCalledTimes(1);
 });
 
 it('should concatenate the passed arguments with the required arguments', async () => {
-    const args = ['--disable-http', '--bind-address', '0.0.0.0'];
-    const launcher = new WiremockLauncher({ ...options, args });
-    launcher.installFile = jest.fn();
+	const args = ['--disable-http', '--bind-address', '0.0.0.0'];
+	const launcher = new WiremockLauncher({ ...options, args });
+	launcher.installFile = jest.fn();
 
-    await launcher.onPrepare();
+	await launcher.onPrepare();
 
-    expect(spawn).toHaveBeenCalledTimes(1);
-    expect(spawn).toHaveBeenCalledWith('java', defaultArgs.concat(args), spawnOptions);
+	expect(spawn).toHaveBeenCalledTimes(1);
+	expect(spawn).toHaveBeenCalledWith('java', defaultArgs.concat(args), spawnOptions);
 });
 
 it('should assign the stopProcess method when watchMode is active', async () => {
-    const callbacks: any[] = [];
-    const mockProcessOn = jest.spyOn(process, 'on').mockImplementation((a: any, b: any): any => {
-        callbacks.push(b);
-    });
-    const launcher = new WiremockLauncher(options, capabilities, { ...wdioConfig, watch: true });
-    launcher['stopProcess'] = jest.fn();
-    launcher.installFile = jest.fn();
-    spawn.mockReturnValue({ on: jest.fn() });
+	const callbacks: any[] = [];
+	const mockProcessOn = jest.spyOn(process, 'on').mockImplementation((a: any, b: any): any => {
+		callbacks.push(b);
+	});
+	const launcher = new WiremockLauncher(options, capabilities, { ...wdioConfig, watch: true });
+	launcher['stopProcess'] = jest.fn();
+	launcher.installFile = jest.fn();
+	spawn.mockReturnValue({ on: jest.fn() });
 
-    await launcher.onPrepare();
-    for (const callback of callbacks) {
-        callback();
-    }
+	await launcher.onPrepare();
+	for (const callback of callbacks) {
+		callback();
+	}
 
-    expect(spawn).toHaveBeenCalledTimes(1);
-    expect(mockProcessOn).toHaveBeenCalledTimes(3);
-    expect(mockProcessOn.mock.calls[0][0]).toEqual('SIGINT');
-    expect(mockProcessOn.mock.calls[1][0]).toEqual('exit');
-    expect(mockProcessOn.mock.calls[2][0]).toEqual('uncaughtException');
-    expect(launcher['stopProcess']).toBeCalledTimes(3);
+	expect(spawn).toHaveBeenCalledTimes(1);
+	expect(mockProcessOn).toHaveBeenCalledTimes(3);
+	expect(mockProcessOn.mock.calls[0][0]).toEqual('SIGINT');
+	expect(mockProcessOn.mock.calls[1][0]).toEqual('exit');
+	expect(mockProcessOn.mock.calls[2][0]).toEqual('uncaughtException');
+	expect(launcher['stopProcess']).toBeCalledTimes(3);
 });
 
 it('should not execute the stopProcess method on completion when watchMode is active', async () => {
-    const launcher = new WiremockLauncher(options, capabilities, { ...wdioConfig, watch: true });
-    launcher.installFile = jest.fn();
-    launcher['stopProcess'] = jest.fn();
-    spawn.mockReturnValue({ on: jest.fn() });
+	const launcher = new WiremockLauncher(options, capabilities, { ...wdioConfig, watch: true });
+	launcher.installFile = jest.fn();
+	launcher['stopProcess'] = jest.fn();
+	spawn.mockReturnValue({ on: jest.fn() });
 
-    await launcher.onPrepare();
-    launcher.onComplete();
+	await launcher.onPrepare();
+	launcher.onComplete();
 
-    expect(spawn).toHaveBeenCalledTimes(1);
-    expect(launcher['stopProcess']).toHaveBeenCalledTimes(0);
+	expect(spawn).toHaveBeenCalledTimes(1);
+	expect(launcher['stopProcess']).toHaveBeenCalledTimes(0);
 });
 
 it('should execute the stopProcess method on completion when watchMode is not active', async () => {
-    const launcher = new WiremockLauncher(options, capabilities, { ...wdioConfig, watch: false });
-    launcher.installFile = jest.fn();
-    launcher['stopProcess'] = jest.fn();
-    spawn.mockReturnValue({ on: jest.fn() });
+	const launcher = new WiremockLauncher(options, capabilities, { ...wdioConfig, watch: false });
+	launcher.installFile = jest.fn();
+	launcher['stopProcess'] = jest.fn();
+	spawn.mockReturnValue({ on: jest.fn() });
 
-    await launcher.onPrepare();
-    launcher.onComplete();
+	await launcher.onPrepare();
+	launcher.onComplete();
 
-    expect(spawn).toHaveBeenCalledTimes(1);
-    expect(launcher['stopProcess']).toHaveBeenCalledTimes(1);
+	expect(spawn).toHaveBeenCalledTimes(1);
+	expect(launcher['stopProcess']).toHaveBeenCalledTimes(1);
 });
 
 it('should throw when waitUntilUsed rejects', async () => {
-    const launcher = new WiremockLauncher();
-    launcher.installFile = jest.fn();
-    spawn.mockReturnValue({ on: jest.fn() });
-    waitUntilUsed.mockRejectedValue(new Error('Error: timeout'));
+	const launcher = new WiremockLauncher();
+	launcher.installFile = jest.fn();
+	spawn.mockReturnValue({ on: jest.fn() });
+	waitUntilUsed.mockRejectedValue(new Error('Error: timeout'));
 
-    await expect(launcher.onPrepare()).rejects.toThrowError('Error: timeout');
+	await expect(launcher.onPrepare()).rejects.toThrowError('Error: timeout');
 });
 
 it('should throw when waitUntilFree rejects', async () => {
-    const launcher = new WiremockLauncher();
-    waitUntilFree.mockRejectedValue(new Error('Error: timeout'));
+	const launcher = new WiremockLauncher();
+	waitUntilFree.mockRejectedValue(new Error('Error: timeout'));
 
-    await expect(launcher.onComplete()).rejects.toThrowError('Error: timeout');
+	await expect(launcher.onComplete()).rejects.toThrowError('Error: timeout');
 });
 
 it('should throw error when trying to set port using the args', async () => {
-    expect(() => new WiremockLauncher({ ...options, args: ['-port', 9999] })).toThrowError(
-        'Cannot set port using args. Use options.port instead.',
-    );
+	expect(() => new WiremockLauncher({ ...options, args: ['-port', 9999] })).toThrowError(
+		'Cannot set port using args. Use options.port instead.',
+	);
 });
 
 it('should throw error when  to set root-dir using the args', async () => {
-    expect(() => new WiremockLauncher({ ...options, args: ['-root-dir', 'dummy'] })).toThrowError(
-        'Cannot set root-dir using args. Use options.rootDir instead.',
-    );
+	expect(() => new WiremockLauncher({ ...options, args: ['-root-dir', 'dummy'] })).toThrowError(
+		'Cannot set root-dir using args. Use options.rootDir instead.',
+	);
 });
--- src/launcher.ts
+++ src/launcher.ts
@@ -6,135 +6,135 @@ import { waitUntilUsed, waitUntilFree } from 'tcp-port-used';
 import { IncomingMessage } from 'http';
 
 function httpRequest(url: string): Promise<IncomingMessage> {
-    return new Promise((resolve, reject) => {
-        const req = get(url, (res) => {
-            if (res.statusCode !== 200) {
-                return reject(new Error('statusCode=' + res.statusCode));
-            }
+	return new Promise((resolve, reject) => {
+		const req = get(url, (res) => {
+			if (res.statusCode !== 200) {
+				return reject(new Error('statusCode=' + res.statusCode));
+			}
 
-            return resolve(res);
-        });
+			return resolve(res);
+		});
 
-        req.on('error', (err) => {
-            reject(err);
-        });
+		req.on('error', (err) => {
+			reject(err);
+		});
 
-        req.end();
-    });
+		req.end();
+	});
 }
 
 export interface Options {
-    port?: number;
-    rootDir?: string;
-    stdio?: StdioOptions;
-    mavenBaseUrl?: string;
-    args?: Array<any>;
-    version?: string;
-    skipWiremockInstall?: boolean;
+	port?: number;
+	rootDir?: string;
+	stdio?: StdioOptions;
+	mavenBaseUrl?: string;
+	args?: Array<any>;
+	version?: string;
+	skipWiremockInstall?: boolean;
 }
 
 export type Capabilities = Array<{ [key: string]: any }> | { [key: string]: { [key: string]: any } };
 
 export interface WdioConfig {
-    watch?: boolean;
+	watch?: boolean;
 }
 
 export interface SpawnOptions {
-    detached: boolean;
-    stdio: StdioOptions;
+	detached: boolean;
+	stdio: StdioOptions;
 }
 
 export class WiremockLauncher {
-    args: Array<any>;
-    port: number;
-    spawnOptions: { stdio: StdioOptions; detached: boolean };
-    url: string;
-    skipWiremockInstall: boolean;
-    watchMode: boolean;
-    binPath: string;
-    process!: ChildProcess | null;
+	args: Array<any>;
+	port: number;
+	spawnOptions: { stdio: StdioOptions; detached: boolean };
+	url: string;
+	skipWiremockInstall: boolean;
+	watchMode: boolean;
+	binPath: string;
+	process!: ChildProcess | null;
 
-    constructor(options: Options = {}, capabilities?: Capabilities, config?: WdioConfig) {
-        const port = options.port || 8080;
-        const rootDir = options.rootDir || 'wiremock';
-        const stdio = options.stdio || 'inherit';
-        const mavenBaseUrl = options.mavenBaseUrl || 'https://repo1.maven.org/maven2';
-        const skipWiremockInstall = options.skipWiremockInstall || false;
-        const args = options.args || [];
-        const version = options.version || '2.26.3';
-        const binPath = resolve(__dirname, `wiremock-standalone-${version}.jar`);
+	constructor(options: Options = {}, capabilities?: Capabilities, config?: WdioConfig) {
+		const port = options.port || 8080;
+		const rootDir = options.rootDir || 'wiremock';
+		const stdio = options.stdio || 'inherit';
+		const mavenBaseUrl = options.mavenBaseUrl || 'https://repo1.maven.org/maven2';
+		const skipWiremockInstall = options.skipWiremockInstall || false;
+		const args = options.args || [];
+		const version = options.version || '2.26.3';
+		const binPath = resolve(__dirname, `wiremock-standalone-${version}.jar`);
 
-        if (!existsSync(rootDir)) {
-            mkdirSync(rootDir, { recursive: true });
-        }
+		if (!existsSync(rootDir)) {
+			mkdirSync(rootDir, { recursive: true });
+		}
 
-        if (args.includes('-port')) {
-            throw new Error('Cannot set port using args. Use options.port instead.');
-        }
+		if (args.includes('-port')) {
+			throw new Error('Cannot set port using args. Use options.port instead.');
+		}
 
-        if (args.includes('-root-dir')) {
-            throw new Error('Cannot set root-dir using args. Use options.rootDir instead.');
-        }
+		if (args.includes('-root-dir')) {
+			throw new Error('Cannot set root-dir using args. Use options.rootDir instead.');
+		}
 
-        this.watchMode = Boolean(config?.watch);
-        this.binPath = binPath;
-        this.port = port;
-        this.spawnOptions = { stdio, detached: true };
-        this.url = `${mavenBaseUrl}/com/github/tomakehurst/wiremock-standalone/${version}/wiremock-standalone-${version}.jar`;
-        this.skipWiremockInstall = !!skipWiremockInstall;
-        this.args = ['-jar', binPath, '-port', port, '-root-dir', rootDir].concat(args);
-    }
+		this.watchMode = Boolean(config?.watch);
+		this.binPath = binPath;
+		this.port = port;
+		this.spawnOptions = { stdio, detached: true };
+		this.url = `${mavenBaseUrl}/com/github/tomakehurst/wiremock-standalone/${version}/wiremock-standalone-${version}.jar`;
+		this.skipWiremockInstall = !!skipWiremockInstall;
+		this.args = ['-jar', binPath, '-port', port, '-root-dir', rootDir].concat(args);
+	}
 
-    async onPrepare() {
-        if (!existsSync(this.binPath) && !this.skipWiremockInstall) {
-            const error = await this.installFile(this.url, this.binPath);
-            if (error) {
-                throw new Error(`Downloading WireMock jar from Maven Central failed: ${error}\n`);
-            }
-        }
+	async onPrepare() {
+		if (!existsSync(this.binPath) && !this.skipWiremockInstall) {
+			const error = await this.installFile(this.url, this.binPath);
+			if (error) {
+				throw new Error(`Downloading WireMock jar from Maven Central failed: ${error}\n`);
+			}
+		}
 
-        this.process = spawn('java', this.args, this.spawnOptions);
+		this.process = spawn('java', this.args, this.spawnOptions);
 
-        this.process.on('error', (error) => {
-            process.stderr.write(error.message);
-        });
+		this.process.on('error', (error) => {
+			process.stderr.write(error.message);
+		});
 
-        this.process.on('exit', (code, signal) => {
-            process.stdout.write(`Wiremock exited with code ${code} ${signal ? 'and signal ' + signal : ''}\n\n`);
-        });
+		this.process.on('exit', (code, signal) => {
+			process.stdout.write(`Wiremock exited with code ${code} ${signal ? 'and signal ' + signal : ''}\n\n`);
+		});
 
-        if (this.watchMode) {
-            process.on('SIGINT', () => this.stopProcess(this.port));
-            process.on('exit', () => this.stopProcess(this.port));
-            process.on('uncaughtException', () => this.stopProcess(this.port));
-        }
+		if (this.watchMode) {
+			process.on('SIGINT', () => this.stopProcess(this.port));
+			process.on('exit', () => this.stopProcess(this.port));
+			process.on('uncaughtException', () => this.stopProcess(this.port));
+		}
 
-        await waitUntilUsed(this.port, 100, 10000);
-    }
+		await waitUntilUsed(this.port, 100, 10000);
+	}
 
-    async onComplete() {
-        if (!this.watchMode) {
-            await this.stopProcess(this.port);
-        }
-    }
+	async onComplete() {
+		if (!this.watchMode) {
+			await this.stopProcess(this.port);
+		}
+	}
 
-    private async stopProcess(port: number) {
-        if (!this.process?.killed) {
-            process.stdout.write('Shutting down wiremock\n');
-            this.process?.kill('SIGTERM');
-        }
+	private async stopProcess(port: number) {
+		if (!this.process?.killed) {
+			process.stdout.write('Shutting down wiremock\n');
+			this.process?.kill('SIGTERM');
+		}
 
-        await waitUntilFree(port, 100, 10000);
-    }
+		await waitUntilFree(port, 100, 10000);
+	}
 
-    async installFile(from: string, to: string): Promise<IncomingMessage> {
-        process.stdout.write(`Downloading WireMock standalone from Maven Central...\n  ${this.url}\n`);
-        const res = await httpRequest(from);
+	async installFile(from: string, to: string): Promise<IncomingMessage> {
+		process.stdout.write(`Downloading WireMock standalone from Maven Central...\n  ${this.url}\n`);
+		const res = await httpRequest(from);
 
-        return new Promise((resolve, reject) => {
-            res.pipe(createWriteStream(to));
-            res.on('end', () => resolve());
-            res.on('error', () => reject(new Error('Could not write to ' + to)));
-        });
-    }
+		return new Promise((resolve, reject) => {
+			res.pipe(createWriteStream(to));
+			res.on('end', () => resolve());
+			res.on('error', () => reject(new Error('Could not write to ' + to)));
+		});
+	}
 }
